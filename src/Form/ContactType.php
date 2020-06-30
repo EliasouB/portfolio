04d365f08894e212de9e93a7cf085482ab57f620
EliasouB <elias.bouabdellah@gmail.com>
2020-06-30 14:53:07 +0200

    refactor clean template

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -13,7 +13,7 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name') 
             ->add('email')
             ->add('tel')
             ->add('content', TextareaType::class)
